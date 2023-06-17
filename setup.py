0cc47c2f2ef898eac052f61d6d53a7a364e1cbcf
my_mac_dimitree54 <dimitree54@gmail.com>
2023-06-17 13:51:12 +0300

    pip setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='yid_langchain_extensions',
+    version='0.1',
+    author='Dmitrii Rashchenko',
+    author_email='dimitree54@gmail.com',
+    packages=find_packages(),
+    description='Useful classes extending langchain library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dimitree54/yid_langchain_extensions',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=required
+)
