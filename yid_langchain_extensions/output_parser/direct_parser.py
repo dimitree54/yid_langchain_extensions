35fd223aa1e8614835a960d0074e28a8c5a2cfad
my_mac_dimitree54 <dimitree54@gmail.com>
2023-06-17 13:53:43 +0300

    flake8

--- yid_langchain_extensions/output_parser/direct_parser.py
+++ yid_langchain_extensions/output_parser/direct_parser.py
@@ -4,6 +4,6 @@ from langchain.schema import BaseOutputParser
 class DirectParser(BaseOutputParser):
     def parse(self, text: str) -> str:
         return text
-    
+
     def get_format_instructions(self) -> str:
         return ""
