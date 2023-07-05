7d072653d46595aee978629811d90f40973ffedf
my_mac_dimitree54 <dimitree54@gmail.com>
2023-07-05 22:11:53 +0300

    extract action thoughts to separate function (to use in child classes)

--- yid_langchain_extensions/output_parser/action_parser.py
+++ yid_langchain_extensions/output_parser/action_parser.py
@@ -8,12 +8,15 @@ from yid_langchain_extensions.output_parser.thoughts_json_parser import Thought,
 
 class ActionParser(ThoughtsJSONParser, AgentOutputParser):
     @classmethod
-    def from_extra_thoughts(cls, extra_thoughts: List[Thought]):
-        action_thoughts: List[Thought] = [
+    def get_action_thoughts(cls) -> List[Thought]:
+        return [
             Thought(name="action", description="The action to take. Must be one of [{{tool_names}}]"),
             Thought(name="action_input", description="The input to the action")
         ]
-        thoughts = extra_thoughts + action_thoughts
+
+    @classmethod
+    def from_extra_thoughts(cls, extra_thoughts: List[Thought]):
+        thoughts = extra_thoughts + cls.get_action_thoughts()
         return cls(thoughts=thoughts)
 
     def parse(self, text: str) -> AgentAction:
