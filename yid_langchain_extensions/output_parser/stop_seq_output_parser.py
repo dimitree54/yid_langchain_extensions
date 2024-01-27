c6c2c001f24a3e5d2db381578aafe3fe9a39a8e0
Dmitrii Rashchenko <dimitree54@gmail.com>
2024-01-27 12:04:10 +0200

    default implementation for stop_sequences

--- yid_langchain_extensions/output_parser/stop_seq_output_parser.py
+++ yid_langchain_extensions/output_parser/stop_seq_output_parser.py
@@ -1,11 +1,10 @@
-from abc import ABC, abstractmethod
-from typing import List
+from abc import ABC
+from typing import List, Optional
 
 from langchain_core.output_parsers import BaseOutputParser
 
 
 class StopSeqOutputParser(BaseOutputParser, ABC):
     @property
-    @abstractmethod
-    def stop_sequences(self) -> List[str]:
-        pass
+    def stop_sequences(self) -> Optional[List[str]]:
+        return None
