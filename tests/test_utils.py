9fbad45e3a4855242caa7e03085b47faf617e2f6
Dmitrii Rashchenko <dimitree54@gmail.com>
2024-06-27 21:38:32 +0300

    FirstMessageAuthorContextSizeLimiter + refactoring of limiters hierarchy (breaking, so bumping)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,11 +3,13 @@ from unittest import TestCase
 
 from langchain_community.llms.fake import FakeListLLM
 from langchain_core.language_models import LLM
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, AIMessage, HumanMessage, SystemMessage
 from langchain_core.runnables import RunnableSerializable
+from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
 
-from yid_langchain_extensions.utils import pydantic_v1_port
+from yid_langchain_extensions.utils import pydantic_v1_port, FirstMessageAuthorContextSizeLimiter, \
+    NaiveContextSizeLimiter
 
 
 class Pydantic2ClassWithoutPort(BaseModel):
@@ -41,3 +43,23 @@ class TestPydanticV1Port(TestCase):
             llm_dict={"llm": self.llm},
             complex_field=self.llm
         )
+
+
+class TestFirstMessageAuthorContextSizeLimiter(TestCase):
+    def test_limit_messages(self):
+        messages = [
+            HumanMessage(content="hi"),
+            SystemMessage(content="say hi"),
+            AIMessage(content="hi"),
+            HumanMessage(content="how r u?"),
+            SystemMessage(content="say how r u"),
+            AIMessage(content="im fine"),
+        ]
+        limiter = FirstMessageAuthorContextSizeLimiter(
+            first_message_author="human",
+            base_limiter=NaiveContextSizeLimiter(
+                max_context_size=40, llm=ChatOpenAI()
+            )
+        )
+        limited_messages = limiter.limit_messages(messages)
+        self.assertEqual(len(limited_messages), 3)
