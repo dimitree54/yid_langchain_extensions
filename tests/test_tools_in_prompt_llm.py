b17516dbe5a51c10d9587a63c83b72f633e9b27f
Dmitrii Rashchenko <dimitree54@gmail.com>
2025-04-27 16:24:40 +0200

    fix useful comments from review rabbit

--- tests/test_tools_in_prompt_llm.py
+++ tests/test_tools_in_prompt_llm.py
@@ -32,13 +32,13 @@ class TestModelWithTools(unittest.TestCase):
             tools=[add, Dot], tool_choice="auto") | DeepseekR1JsonToolCallsParser()
 
         answer: AIMessage = chain_sequential.invoke("hi")
-        self.assertEquals(len(answer.tool_calls), 0)
+        self.assertEqual(len(answer.tool_calls), 0)
 
         answer: AIMessage = chain_sequential.invoke("call tool add(3,5)")
-        self.assertEquals(len(answer.tool_calls), 1)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
-        self.assertEquals(answer.tool_calls[0]["args"].get("a"), 3)
-        self.assertEquals(answer.tool_calls[0]["args"].get("b"), 5)
+        self.assertEqual(len(answer.tool_calls), 1)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(answer.tool_calls[0]["args"].get("a"), 3)
+        self.assertEqual(answer.tool_calls[0]["args"].get("b"), 5)
         self.assertIsNotNone(answer.tool_calls[0]["id"])
 
     def test_auto_sequential_tool_choice_tricky(self):
@@ -46,72 +46,72 @@ class TestModelWithTools(unittest.TestCase):
             tools=[add, Dot], tool_choice="auto") | DeepseekR1JsonToolCallsParser()
 
         answer: AIMessage = chain_sequential.invoke("call tool add(3,5) and Dot(2,7)")
-        self.assertEquals(len(answer.tool_calls), 1)
+        self.assertEqual(len(answer.tool_calls), 1)
 
     def test_auto_parallel_tool_choice(self):
         chain_parallel = self.llm.bind_tools(
             tools=[add, Dot], tool_choice="auto", parallel_tool_calls=True) | DeepseekR1JsonToolCallsParser()
 
         answer: AIMessage = chain_parallel.invoke("hi")
-        self.assertEquals(len(answer.tool_calls), 0)
+        self.assertEqual(len(answer.tool_calls), 0)
 
         answer: AIMessage = chain_parallel.invoke("call tool add(3,5)")
-        self.assertEquals(len(answer.tool_calls), 1)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
-        self.assertEquals(answer.tool_calls[0]["args"].get("a"), 3)
-        self.assertEquals(answer.tool_calls[0]["args"].get("b"), 5)
+        self.assertEqual(len(answer.tool_calls), 1)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(answer.tool_calls[0]["args"].get("a"), 3)
+        self.assertEqual(answer.tool_calls[0]["args"].get("b"), 5)
         self.assertIsNotNone(answer.tool_calls[0]["id"])
 
         answer: AIMessage = chain_parallel.invoke("call tool add(3,5) and Dot(2,7)")
-        self.assertEquals(len(answer.tool_calls), 2)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
-        self.assertEquals(answer.tool_calls[0]["args"].get("a"), 3)
-        self.assertEquals(answer.tool_calls[0]["args"].get("b"), 5)
+        self.assertEqual(len(answer.tool_calls), 2)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(answer.tool_calls[0]["args"].get("a"), 3)
+        self.assertEqual(answer.tool_calls[0]["args"].get("b"), 5)
         self.assertIsNotNone(answer.tool_calls[0]["id"])
-        self.assertEquals(len(answer.tool_calls), 2)
-        self.assertEquals(answer.tool_calls[1]["name"], "Dot")
-        self.assertEquals(answer.tool_calls[1]["args"].get("a"), 2)
-        self.assertEquals(answer.tool_calls[1]["args"].get("b"), 7)
+        self.assertEqual(len(answer.tool_calls), 2)
+        self.assertEqual(answer.tool_calls[1]["name"], "Dot")
+        self.assertEqual(answer.tool_calls[1]["args"].get("a"), 2)
+        self.assertEqual(answer.tool_calls[1]["args"].get("b"), 7)
         self.assertIsNotNone(answer.tool_calls[1]["id"])
 
     def test_any_sequential_tool_choice(self):
         chain_sequential = self.llm.bind_tools(tools=[add, Dot], tool_choice="any") | DeepseekR1JsonToolCallsParser()
 
         answer: AIMessage = chain_sequential.invoke("hi")
-        self.assertEquals(len(answer.tool_calls), 1)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(len(answer.tool_calls), 1)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
 
         answer: AIMessage = chain_sequential.invoke("call tool add(3,5)")
-        self.assertEquals(len(answer.tool_calls), 1)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
-        self.assertEquals(answer.tool_calls[0]["args"].get("a"), 3)
-        self.assertEquals(answer.tool_calls[0]["args"].get("b"), 5)
+        self.assertEqual(len(answer.tool_calls), 1)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(answer.tool_calls[0]["args"].get("a"), 3)
+        self.assertEqual(answer.tool_calls[0]["args"].get("b"), 5)
         self.assertIsNotNone(answer.tool_calls[0]["id"])
 
         answer: AIMessage = chain_sequential.invoke("call tool add(3,5) and Dot(2,7)")
-        self.assertEquals(len(answer.tool_calls), 1)
+        self.assertEqual(len(answer.tool_calls), 1)
 
     def test_any_parallel_tool_choice(self):
         chain_parallel = self.llm.bind_tools(
             tools=[add, Dot], tool_choice="any", parallel_tool_calls=True) | DeepseekR1JsonToolCallsParser()
 
         answer: AIMessage = chain_parallel.invoke("call tool add(3,5)")
-        self.assertEquals(len(answer.tool_calls), 1)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
-        self.assertEquals(answer.tool_calls[0]["args"].get("a"), 3)
-        self.assertEquals(answer.tool_calls[0]["args"].get("b"), 5)
+        self.assertEqual(len(answer.tool_calls), 1)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(answer.tool_calls[0]["args"].get("a"), 3)
+        self.assertEqual(answer.tool_calls[0]["args"].get("b"), 5)
         self.assertIsNotNone(answer.tool_calls[0]["id"])
 
         answer: AIMessage = chain_parallel.invoke("call tool add(3,5) and Dot(2,7)")
-        self.assertEquals(len(answer.tool_calls), 2)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
-        self.assertEquals(answer.tool_calls[0]["args"].get("a"), 3)
-        self.assertEquals(answer.tool_calls[0]["args"].get("b"), 5)
+        self.assertEqual(len(answer.tool_calls), 2)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(answer.tool_calls[0]["args"].get("a"), 3)
+        self.assertEqual(answer.tool_calls[0]["args"].get("b"), 5)
         self.assertIsNotNone(answer.tool_calls[0]["id"])
-        self.assertEquals(len(answer.tool_calls), 2)
-        self.assertEquals(answer.tool_calls[1]["name"], "Dot")
-        self.assertEquals(answer.tool_calls[1]["args"].get("a"), 2)
-        self.assertEquals(answer.tool_calls[1]["args"].get("b"), 7)
+        self.assertEqual(len(answer.tool_calls), 2)
+        self.assertEqual(answer.tool_calls[1]["name"], "Dot")
+        self.assertEqual(answer.tool_calls[1]["args"].get("a"), 2)
+        self.assertEqual(answer.tool_calls[1]["args"].get("b"), 7)
         self.assertIsNotNone(answer.tool_calls[1]["id"])
 
     def test_any_parallel_tricky(self):
@@ -127,33 +127,33 @@ class TestModelWithTools(unittest.TestCase):
         chain_sequential = self.llm.bind_tools(tools=[add, Dot], tool_choice="add") | DeepseekR1JsonToolCallsParser()
 
         answer: AIMessage = chain_sequential.invoke("hi")
-        self.assertEquals(len(answer.tool_calls), 1)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(len(answer.tool_calls), 1)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
 
         answer: AIMessage = chain_sequential.invoke("call tool add(3,5)")
-        self.assertEquals(len(answer.tool_calls), 1)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
-        self.assertEquals(answer.tool_calls[0]["args"].get("a"), 3)
-        self.assertEquals(answer.tool_calls[0]["args"].get("b"), 5)
+        self.assertEqual(len(answer.tool_calls), 1)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(answer.tool_calls[0]["args"].get("a"), 3)
+        self.assertEqual(answer.tool_calls[0]["args"].get("b"), 5)
         self.assertIsNotNone(answer.tool_calls[0]["id"])
 
         answer: AIMessage = chain_sequential.invoke("call tool add(3,5) and Dot(2,7)")
-        self.assertEquals(len(answer.tool_calls), 1)
+        self.assertEqual(len(answer.tool_calls), 1)
 
     def test_specific_parallel_tool_choice(self):
         chain_parallel = self.llm.bind_tools(
             tools=[add, Dot], tool_choice="add", parallel_tool_calls=True) | DeepseekR1JsonToolCallsParser()
 
         answer: AIMessage = chain_parallel.invoke("call tool add(3,5) and add(2,7)")
-        self.assertEquals(len(answer.tool_calls), 2)
-        self.assertEquals(answer.tool_calls[0]["name"], "add")
-        self.assertEquals(answer.tool_calls[0]["args"].get("a"), 3)
-        self.assertEquals(answer.tool_calls[0]["args"].get("b"), 5)
+        self.assertEqual(len(answer.tool_calls), 2)
+        self.assertEqual(answer.tool_calls[0]["name"], "add")
+        self.assertEqual(answer.tool_calls[0]["args"].get("a"), 3)
+        self.assertEqual(answer.tool_calls[0]["args"].get("b"), 5)
         self.assertIsNotNone(answer.tool_calls[0]["id"])
-        self.assertEquals(len(answer.tool_calls), 2)
-        self.assertEquals(answer.tool_calls[1]["name"], "add")
-        self.assertEquals(answer.tool_calls[1]["args"].get("a"), 2)
-        self.assertEquals(answer.tool_calls[1]["args"].get("b"), 7)
+        self.assertEqual(len(answer.tool_calls), 2)
+        self.assertEqual(answer.tool_calls[1]["name"], "add")
+        self.assertEqual(answer.tool_calls[1]["args"].get("a"), 2)
+        self.assertEqual(answer.tool_calls[1]["args"].get("b"), 7)
         self.assertIsNotNone(answer.tool_calls[1]["id"])
 
     def test_specific_parallel_tool_choice_tricky(self):
@@ -163,4 +163,4 @@ class TestModelWithTools(unittest.TestCase):
         answer: AIMessage = chain_parallel.invoke("hi")
         self.assertGreater(len(answer.tool_calls), 0)
         for tool_call in answer.tool_calls:
-            self.assertIn(tool_call["name"], {"add", "Dot"})
+            self.assertEqual(tool_call["name"], "add")
