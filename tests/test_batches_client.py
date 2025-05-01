3bd26f31d63dd5dfbbb61adece270e3102f2c89c
Dmitrii Rashchenko <dimitree54@gmail.com>
2025-05-01 09:37:43 +0200

    clean up lib and bump version

--- tests/test_batches_client.py
+++ tests/test_batches_client.py
@@ -10,36 +10,42 @@ from yid_langchain_extensions.llm.batches_openai_client import BatchesOpenAIComp
 from yid_langchain_extensions.utils import encode_image_to_url
 
 
-class TestBatchesClient(unittest.IsolatedAsyncioTestCase):
-    def setUp(self) -> None:
-        batches_client = BatchesOpenAICompletions()
-        self.llm = ChatOpenAI(model_name="gpt-4.1-nano", temperature=0, async_client=batches_client)
+def build_llm():
+    batches_client = BatchesOpenAICompletions()
+    return ChatOpenAI(model_name="gpt-4.1-nano", temperature=0, async_client=batches_client)
 
+
+def generate_random_image():
+    random_image_array = np.random.randint(0, 256, (128, 128, 3), dtype=np.uint8)
+    return random_image_array
+
+
+class TestBatchesClientInChain(unittest.IsolatedAsyncioTestCase):
     async def test_batches_client_in_chain(self):
         prompt = ChatPromptTemplate.from_messages([HumanMessage(content="{message}")])
-        chain = prompt | self.llm | StrOutputParser()
+        chain = prompt | build_llm() | StrOutputParser()
         answer = await chain.ainvoke(input={"message": "hi"})
         self.assertTrue(len(answer) > 0)
 
-    def generate_random_image(self):
-        random_image_array = np.random.randint(0, 256, (128, 128, 3), dtype=np.uint8)
-        return random_image_array
 
+class TestBatchesClientWithInlineImage(unittest.IsolatedAsyncioTestCase):
     async def test_batches_client_with_inline_image(self):
-        chain = self.llm | StrOutputParser()
+        chain = build_llm() | StrOutputParser()
 
         content = [
             {'type': 'text', 'text': 'hi'},
             {'type': 'image_url', 'image_url': {
-                'url': encode_image_to_url(self.generate_random_image())
+                'url': encode_image_to_url(generate_random_image())
             }}
         ]
 
         answer = await chain.ainvoke(input=[HumanMessage(content=content)])
         self.assertTrue(len(answer) > 0)
 
+
+class TestBatchesClientWithOnlineImage(unittest.IsolatedAsyncioTestCase):
     async def test_batches_client_with_online_image(self):
-        chain = self.llm | StrOutputParser()
+        chain = build_llm() | StrOutputParser()
 
         url = ("https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/"
                "Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg")
--- yid_langchain_extensions/output_parser/direct_parser.py
+++
@@ -1,9 +0,0 @@
-from langchain.schema import BaseOutputParser
-
-
-class DirectParser(BaseOutputParser):
-    def parse(self, text: str) -> str:
-        return text
-
-    def get_format_instructions(self) -> str:
-        return ""
